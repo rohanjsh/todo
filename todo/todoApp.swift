7db3ecf90c7b5adfcb6d356020eeafb2bb9342e2
Rohan <rohan.joshi@goodcreator.co>
2024-02-07 18:52:13 +0530

    Initial Commit

---
+++ todo/todoApp.swift
@@ -0,0 +1,17 @@
+//
+//  todoApp.swift
+//  todo
+//
+//  Created by Rohan @Winkl on 07/02/24.
+//
+
+import SwiftUI
+
+@main
+struct todoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
